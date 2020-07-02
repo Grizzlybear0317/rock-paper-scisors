c2369793d16b49db5892fe00d58c3455984ea9c1
shakirshuvo <shakirshuvo@hotmail.com>
2020-07-02 01:22:07 -0700

    two fixes
    2 fixes

--- TestGame.py
+++ TestGame.py
@@ -1,44 +1,53 @@
 import RockPaperScissors
 
+
 def test_player_tie_with_r():
     player_input = "r"
     cpu_input = "r"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "tie"
+
 
 def test_player_win_with_r():
     player_input = "r"
     cpu_input = "s"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "win"
 
+
 def test_player_lose_with_r():
     player_input = "r"
     cpu_input = "p"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "lose"
+
 
 def test_player_tie_with_p():
     player_input = "p"
     cpu_input = "p"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "tie"
 
+
 def test_player_win_with_p():
-    player_input = "P"
+    player_input = "p"
     cpu_input = "r"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "win"
+
 
 def test_player_lose_with_p():
     player_input = "p"
     cpu_input = "s"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "lose"
 
+
 def test_player_tie_with_s():
     player_input = "s"
     cpu_input = "s"
     assert RockPaperScissors.get_player_result(player_input, cpu_input) == "tie"
 
+
 def test_player_win_with_s():
     player_input = "s"
     cpu_input = "p"
-    assert RockPaperScissors.get_player_result(player_input, cpu_input) == "tie"
+    assert RockPaperScissors.get_player_result(player_input, cpu_input) == "win"
+
 
 def test_player_lose_with_s():
     player_input = "s"
