f96eee94e685576b3e6df043005b10fe9f8e6236
shakirshuvo <shakirshuvo@hotmail.com>
2020-07-02 01:26:17 -0700

    Fixed 2 unit tests
    Fixed 2 unit tests

--- game/RockPaperScissors.py
+++ game/RockPaperScissors.py
@@ -1,21 +1,7 @@
 import random
 
-comp_wins = 0
-player_wins = 0
-
-
 def play_game():
-    # user_choice = input("Choose Rock, Paper or Scissors: ")
-    # if user_choice in ["Rock", "rock", "r", "R"]:
-    #     user_choice = "r"
-    # elif user_choice in ["Paper", "paper", "p", "P"]:
-    #     user_choice = "p"
-    # elif user_choice in ["Scissors", "scissors", "s", "S"]:
-    #     user_choice = "s"
-    # else:
-    #     print("I don't understand, try again.")
-    #     Choose_option()
-    # return user_choice
+
     user_input = input("Choose either rock(r), paper(p) or scissors(s): ")
     if not verify_input(user_input):
         print("Invalid input. Please enter either 'r' for rock, 'p' for paper, or 's' for scissors")
@@ -68,39 +54,33 @@ while True:
 
         elif comp_choice == "p":
             print("You chose rock. The computer chose paper. You lose.")
-            comp_wins += 1
 
         elif comp_choice == "s":
             print("You chose rock. The computer chose scissors. You win.")
-            player_wins += 1
 
         elif user_choice == "p":
             if comp_choice == "r":
                 print("You chose paper. The computer chose rock. You win.")
-                player_wins += 1
 
         elif comp_choice == "p":
             print("You chose paper. The computer chose paper. You tied.")
 
         elif comp_choice == "s":
             print("You chose paper. The computer chose scissors. You lose.")
-            comp_wins += 1
 
         elif user_choice == "s":
             if comp_choice == "r":
                 print("You chose scissors. The computer chose rock. You lose.")
-                comp_wins += 1
 
         elif comp_choice == "p":
             print("You chose scissors. The computer chose paper. You win.")
-            player_wins += 1
 
         elif comp_choice == "s":
             print("You chose scissors. The computer chose scissors. You tied.")
 
         print("")
-        print("Player wins: " + str(player_wins))
-        print("Computer wins: " + str(comp_wins))
+        print("Player wins")
+        print("Computer wins")
         print("")
 
         user_choice = input("Do you want to play again? (y/n)")
